38af5c3df3dd7de7e72c5480d3c3231e0654615a
hamin7 <33690039+hamin7@users.noreply.github.com>
2018-10-05 22:06:01 +0900

    Add files via upload

---
+++ chap06/src/sec06/exam01_field_declaration/Car.java
@@ -0,0 +1,9 @@
+package sec06.exam01_field_declaration;
+
+public class Car {
+	String company ="현대자동차";  //클래스 중괄호 안에 변수선언과 같이 선언된것 = 필
+	String model = "그랜저";
+	String color = "검정";
+	int macSpeed = 350;
+	int speed; // 초기값 주지 않으면 기본적으로 0 
+}
---
+++ chap06/src/sec06/exam01_field_declaration/CarExample.java
@@ -0,0 +1,10 @@
+package sec06.exam01_field_declaration;
+
+public class CarExample {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
